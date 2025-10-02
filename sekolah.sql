385e38b81d2a2eb495cea2691c842ce68157c990
Nisrina Nur Fatin <nisrinanurfatin@gmail.com>
2025-10-02 15:29:23 +0700

    Menambahkan data siswa dan nilai

--- sekolah.sql
+++ sekolah.sql
@@ -1,14 +1,17 @@
-create table siswa (
-	id SERIAL primary key,
-	nama VARCHAR(100) not null,
-	umur INT,
-	jurusan VARCHAR(50)
-);
+insert into siswa (nama, umur, jurusan)
+values 
+	('Andi', 16, 'IPA'), 
+	('Budi', 17, 'IPS'), 
+	('Pratama', 17, 'IPA'), 
+	('Lestari', 17, 'IPS'), 
+	('Citra', 17, 'IPA'), 
+	('Dewi', 17, 'IPS');
 
-create table nilai (
-	id SERIAL primary key,
-	siswa_id serial4 not null,
-	foreign key (siswa_id) references siswa(id),
-	mata_pelajaran VARCHAR(100),
-	nilai INT
-);
+insert into nilai (siswa_id, mata_pelajaran, nilai)
+values
+	(1, 'Matematika', 85),
+	(2, 'Bahasa Inggris', 90),
+	(3, 'Bahasa Indonesia', 90),
+	(4, 'IPS', 90),
+	(5, 'Seni Budaya', 86),
+	(6, 'PJOK', 90);
